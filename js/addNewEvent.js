c4f6a650c5796cbd7fbdc1f96d6b0f6a65cde9ce
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-04-28 20:54:14 +0200

    Add .editorconfig and fix files.

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -1,18 +1,18 @@
 /* --------------------------
 * UPDATE UPCOMING EVENT DATE
-* -------------------------- 
+* --------------------------
 *
 * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
 *
 */
-const 
+const
 // Format: YYYY (2020)
 year = 2019,
-// Format: MM (09) or M (9), both are valid 
+// Format: MM (09) or M (9), both are valid
 month = 12,
 // Format: DD (09) or D (9), both are valid
-day = 2,  
-// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
+day = 2,
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 hour = 13,
 // Format: MM (09) or M (9), both are valid
 minute = 00;
