2c81da11da001d1862502209b6d5dda74c4652e0
greg-bath <51409103+greg-bath@users.noreply.github.com>
2020-10-06 21:19:13 +0200

    Update addNewEvent.js

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -9,9 +9,9 @@ const
 // Format: YYYY (2020)
 year = 2020,
 // Format: MM (09) or M (9), both are valid 
-month = 09,
+month = 10,
 // Format: DD (09) or D (9), both are valid
-day = 15  
+day = 13  
 // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
 hour = 10,
 // Format: MM (09) or M (9), both are valid
@@ -21,4 +21,4 @@ minute = 00;
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-const eventName = 'Apple September Event';
+const eventName = 'Apple October Event';
