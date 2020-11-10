50774663a5c69e6a53b0fdf352ad1c330b02c5cc
Tarik02 <Taras.Fomin@gmail.com>
2020-11-10 21:55:06 +0200

    Use vanilla js instead of jquery

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -1,24 +1,26 @@
 /* --------------------------
 * UPDATE UPCOMING EVENT DATE
-* -------------------------- 
+* --------------------------
 *
 * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
 *
 */
-const 
-// Format: YYYY (2020)
-year = 2020,
-// Format: MM (09) or M (9), both are valid 
-month = 11,
-// Format: DD (09) or D (9), both are valid
-day = 10  
-// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
-hour = 10,
-// Format: MM (09) or M (9), both are valid
-minute = 00;
+const
+  // Format: YYYY (2020)
+  EVENT_YEAR = 2020,
+  // Format: MM (09) or M (9), both are valid
+  EVENT_MONTH = 11,
+  // Format: DD (09) or D (9), both are valid
+  EVENT_DAY = 10
+  // Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
+  EVENT_HOUR = 10,
+  // Format: MM (09) or M (9), both are valid
+  EVENT_MINUTE = 00,
+  // Format: milliseconds
+  EVENT_DURATION = 2 * 60 * 60 * 1000; // 2 hours
 
 /* --------------------------
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-const eventName = 'One more thing';
+const EVENT_NAME = 'One more thing';
