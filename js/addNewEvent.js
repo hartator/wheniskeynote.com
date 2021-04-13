d4ee063c2bc83c7adc77c1a417c9ad73df53293b
Vogel, Philipp <vogelphilipp@icloud.com>
2021-04-13 18:16:08 +0200

    updated for spring event

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -1,18 +1,18 @@
 /* --------------------------
 * UPDATE UPCOMING EVENT DATE
-* -------------------------- 
+* --------------------------
 *
 * IMPORTANT: The date has to be in the Pacific Standard Time (PST) zone
 *
 */
-const 
+const
 // Format: YYYY (2020)
 year = 2021,
-// Format: MM (09) or M (9), both are valid 
-month = 06,
+// Format: MM (09) or M (9), both are valid
+month = 04,
 // Format: DD (09) or D (9), both are valid
-day = 07,  
-// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM 
+day = 20,
+// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 hour = 10,
 // Format: MM (09) or M (9), both are valid
 minute = 00;
@@ -21,4 +21,4 @@ minute = 00;
 * UPDATE UPCOMING EVENT NAME
 * --------------------------
 */
-const eventName = 'WWDC 2021';
+const eventName = 'Spring Loaded.';
