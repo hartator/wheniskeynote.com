6f8e077a6c2075f56537a649f478eaaf9059b7e4
Francis Yeo <francisyzy@me.com>
2021-10-13 00:23:47 +0800

    Add Unleashed Event
    https://techcrunch.com/2021/10/12/apple-is-going-unleashed-for-an-october-18-event/

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -11,9 +11,9 @@ const // The time zone when the event will be held. Format: time zone identifier
 	year = 2021,
 	// The month as a number, not the index
 	// Format: MM (09) or M (9), both are valid
-	month = 09,
+	month = 10,
 	// Format: DD (09) or D (9), both are valid
-	day = 14,
+	day = 18,
 	// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 	hour = 10,
 	// Format: MM (09) or M (9), both are valid
@@ -23,4 +23,4 @@ const // The time zone when the event will be held. Format: time zone identifier
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = 'Apple Event';
+const eventName = 'Unleashed';
