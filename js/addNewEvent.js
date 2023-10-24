95c5e4263161023677110ca3644e6f37190c05d4
Francis Yeo <github@francisyzy.com>
2023-10-25 01:17:01 +0800

    Add apple october event
    https://www.apple.com/apple-events/
    
    https://www.theverge.com/2023/10/24/23930407/apple-scary-fast-halloween-imac-event-macbook-pro-rumors
    
    https://www.macrumors.com/2023/10/24/apple-october-mac-event-2023/

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -11,11 +11,11 @@ const // The time zone when the event will be held. Format: time zone identifier
 	year = 2023,
 	// The month as a number, not the index
 	// Format: MM (09) or M (9), both are valid
-	month = 09,
+	month = 10,
 	// Format: DD (09) or D (9), both are valid
-	day = 12,
+	day = 30,
 	// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
-	hour = 10,
+	hour = 17,
 	// Format: MM (09) or M (9), both are valid
 	minute = 00;
 
@@ -23,4 +23,4 @@ const // The time zone when the event will be held. Format: time zone identifier
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = 'Apple Event - September 2023';
+const eventName = 'Apple Event - October 2023';
