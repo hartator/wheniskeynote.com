66abc7cf1fd9200a5b55dbf9fef8b71144018a34
Luca Ban <mesqueeb@users.noreply.github.com>
2026-03-24 07:28:49 +1300

    WWDC26 event

--- js/addNewEvent.js
+++ js/addNewEvent.js
@@ -11,9 +11,9 @@ const // The time zone when the event will be held. Format: time zone identifier
 	year = 2026,
 	// The month as a number, not the index
 	// Format: MM (09) or M (9), both are valid
-	month = 03,
+	month = 06,
 	// Format: DD (09) or D (9), both are valid
-	day = 04,
+	day = 08,
 	// Format: 0 - 24 / 5 == 5 AM / 17 == 5 PM
 	hour = 09,
 	// Format: MM (09) or M (9), both are valid
@@ -23,4 +23,4 @@ const // The time zone when the event will be held. Format: time zone identifier
  * UPDATE UPCOMING EVENT NAME
  * --------------------------
  */
-const eventName = "You're invited.";
+const eventName = "WWDC26";
